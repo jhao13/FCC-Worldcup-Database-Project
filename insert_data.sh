eb570bb2a9e52d5bde7ac47f0b26f6390f771ef9
redlessblade <zzzjhao@gmail.com>
2024-10-11 21:57:52 -0400

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -12,30 +12,49 @@ fi
 echo $($PSQL "TRUNCATE games, teams RESTART IDENTITY")
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT W_GOALS O_GOALS #IFS reads until comma to put seperate words into variableS
 do
-  if [[ $YEAR != "year" ]]
+  if [[ $WINNER != "winner" ]]
     then
     #get winner_id to put into names column for teams table
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     #if not found
     if [[ -z $WINNER_ID ]] #CHECKS IF WINNER_ID IS EMPTY, INSERTS RESULTS INTO DATABASE IF ID IS EMPTY/NONEXISTENT
       then
-      $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")   
+      INSERT_WINNER_TEAM_NAME=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")   
+        if [[ $INSERT_WINNER_TEAM_NAME == "INSERT 0 1" ]]
+          then
+            echo Inserted team $WINNER
+        fi
       #echo "Inserted into name column of teams table, $WINNER"
       #get new team ids
       #WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
-
+  fi
+  
+  if [[ $OPPONENT != "opponent" ]]
+    then
     #get opponent_id to put into names column for teams table
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     if [[ -z $OPPONENT_ID ]] #CHECKS IF id IS EMPTY, INSERTS RESULTS INTO DATABASE IF ID IS EMPTY/NONEXISTENT
       then
-      $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")   
+      INSERT_OPPONENT_TEAM_NAME=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")   
+        if [[ $INSERT_OPPONENT_TEAM_NAME == "INSERT 0 1" ]]
+          then
+            echo Inserted team $OPPONENT
+        fi
       #echo "Inserted into name column of teams table, $OPPONENT"
       #get new team ids
       #OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     fi
+  fi
+  if [[ YEAR != "year" ]]
+    then
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    $($PSQL "INSERT INTO games(winner_id, opponent_id, winner_goals, opponent_goals, year, round) VALUES($WINNER_ID, $OPPONENT_ID, $W_GOALS, $O_GOALS, $YEAR, '$ROUND')")   
+    INSERT_GAME=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $W_GOALS, $O_GOALS)")
+      if [[ $INSERT_GAME == "INSERT 0 1" ]]
+        then
+          echo New game added: $YEAR, $ROUND, $WINNER_ID VS $OPPONENT_ID, score $W_GOALS : $O_GOALS
+      fi
   fi
+  
 done
