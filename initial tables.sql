be1b0640418a3c26840546366ed1ff93e9963e83
J <zzzjhao@gmail.com>
2024-10-20 22:11:33 -0400

    Update initial tables.sql

--- initial tables.sql
+++ initial tables.sql
@@ -7,9 +7,9 @@ USE worldcup
 /* create table 'game' to store the games information, identity(1,1) for starting value of ID and increment*/
 
 CREATE TABLE games (
-    game_id integer NOT NULL identity(1,1),
+    game_id integer NOT NULL identity(1,1,
     year integer NOT NULL,
-    round character varying(40) NOT NULL,
+    round character varying(50) NOT NULL,
     winner_id integer NOT NULL,
     opponent_id integer NOT NULL,
     winner_goals integer NOT NULL,
@@ -20,30 +20,28 @@ CREATE TABLE games (
 
 CREATE TABLE teams (
     team_id integer NOT NULL identity(1,1),
-    name character varying(40) NOT NULL
+    name character varying(50) NOT NULL
 );
 
 /* add unique constraint on team names */
 
 ALTER TABLE teams
-    ADD UNIQUE (name);
+    ADD CONSTRAINT teams_name_key UNIQUE (name);
 
 /* add primary key constraints */
 
 ALTER TABLE games
-    ADD PRIMARY KEY (game_id);
+    ADD CONSTRAINT games_pkey PRIMARY KEY (game_id);
 
-ALTER TABLE teams
-    ADD PRIMARY KEY (team_id);
+ALTER TABLE public.teams
+    ADD CONSTRAINT teams_pkey PRIMARY KEY (team_id);
 
 /* add foreign keys */
 
 ALTER TABLE games
-    ADD FOREIGN KEY (opponent_id) 
-	REFERENCES teams(team_id);
+    ADD CONSTRAINT fk_winner_id FOREIGN KEY (winner_id) REFERENCES teams(team_id);
 
 ALTER TABLE games
-    ADD FOREIGN KEY (winner_id) 
-	REFERENCES teams(team_id);
+    ADD CONSTRAINT fkopponent_id FOREIGN KEY (opponent_id) REFERENCES teams(team_id);
 
 /* tableas are empty and data will be inserted by running the script insert_data.sh */
